6d6c1707817f8976538d0abc0741e806a0a1af35
99bytes <ayushsinha6206@gmail.com>
2025-07-28 13:43:19 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from "react"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Analytics from "./Components/Analytics"
+import Newsletter from "./Components/Newsletter"
+import Cards from "./Components/Cards"
+import Footer from "./Components/footer"
+function App() {
+  
+
+  return (
+   <div>
+    <Navbar/>
+    <Hero/>
+    <Analytics/>
+    <Newsletter/>
+    <Cards/>
+    <Footer/>
+  </div>
+  )
+}
+
+export default App
